67af6b6ca604c4cbef79f74b9655c3564d3976ce
Eduardo Frois <126896990+FroisPeek@users.noreply.github.com>
2023-07-27 22:01:06 -0300

    Add files via upload

---
+++ 03.py
@@ -0,0 +1,12 @@
+"""
+for n in range(0, 11, 1):
+    print(n*2)
+"""
+"""
+x = int(input("Informe a saida: "))
+soma = 0
+for n in range(0, x+1, 1):
+    print(n)
+    soma += n
+print(f"A soma de todos valores: {soma}")
+"""
