7332b287c956ef64ff9682f25c36cf38e87d2a13
Eduardo Frois <126896990+FroisPeek@users.noreply.github.com>
2023-07-27 22:02:29 -0300

    Add files via upload

---
+++ Exercicios/04.py
@@ -0,0 +1,14 @@
+"""
+soma = 0
+for i in range(0, 10+1, 1):
+    print(i*2)
+    soma += i*2
+print(f"A soma dos numeros: {soma}")
+"""
+"""
+produto = 1
+for i in range(0, 2+1, 1):
+    x = int(input("Informe um valor: "))
+    produto *= x
+print(f"O produto dos valores é: {produto}")
+"""
---
+++ Exercicios/05.py
@@ -0,0 +1,25 @@
+# 1 =
+#def mostrar_msg(msg, numero):
+#    print(msg, numero)
+#
+#def main():
+#    msg = "Hello World!"
+#    numero = 45
+#    mostrar_msg(msg, numero)
+#if __name__ == '__main__':
+#    main()
+
+
+
+#2 =
+#def multiplicacao(v1, v2):
+#    mult = v1 * v2
+#    print(f"O resultado é: {mult}")
+#
+#def main():
+#    v1 = int(input("Informe o primeiro valor para a multiplicacao: "))
+#    v2 = int(input("Informe o segundo valor para a multiplicacao: "))
+#    multiplicacao(v1, v2)
+#
+#if __name__ == '__main__':
+#    main()
