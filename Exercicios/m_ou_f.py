c67a7407b56315fdc9089ef8a1f16b4c9dc641a7
Eduardo Frois <126896990+FroisPeek@users.noreply.github.com>
2023-08-01 10:34:07 -0300

    Update m_ou_f.py

--- Exercicios/m_ou_f.py
+++ Exercicios/m_ou_f.py
@@ -1,5 +1,5 @@
 l_altura = []
-l_genereo = []
+l_genero = []
 print("Digite [0] para sair do programa")
 while True:
     altura = float(input("Informe a altura: "))
@@ -8,9 +8,11 @@ while True:
     l_altura.append(altura)
 
     genero = input("Informe o genere, [f] para feminino e [m] para masculino: ")
-    l_genereo.append(genero)
+    l_genero.append(genero)
 
 print(f"A maior altura: {max(l_altura)}")
 print(f"A menor altura: {min(l_altura)}")
-print("A quantidade de homens: ", l_genereo.count("m"))
-print("A quantidade de mulheres: ", l_genereo.count("f"))
+print("A quantidade de homens: ", l_genero.count("m"))
+print("A quantidade de mulheres: ", l_genero.count("f"))
+print(f"A media das alturas: {(sum(l_altura) / len(l_altura)):.2f}")
+print("A porcentagem de homens: ", l_genero.count("m") / len(l_genero) * 100)
