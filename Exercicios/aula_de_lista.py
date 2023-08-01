0e6d7e60fd9070557719ba0240ef52a486df30bd
Eduardo Frois <126896990+FroisPeek@users.noreply.github.com>
2023-08-01 08:43:16 -0300

    Add files via upload

---
+++ Exercicios/aula_de_lista.py
@@ -0,0 +1,12 @@
+lista_nota = []
+nota = 0
+print("Insira [-1] para sair do programa! ")
+while True:
+    nota = float(input("Informe a nota do aluno: "))
+    if nota == -1:
+        break
+    else:
+        lista_nota.append(nota)
+
+print("A media da turma: ", sum(lista_nota) / len(lista_nota))
+print("A quantidade de alunos: ", len(lista_nota))
